587e33dfddf5fe9202b06a49979fa97c7dca5b62
Liam Burnand <liam.burnand@ystv.co.uk>
2023-10-22 19:30:02 +0100

    Adding initial database stuff for Player

---
+++ player/player.go
@@ -0,0 +1,57 @@
+package player
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/guregu/null.v4"
+)
+
+type (
+	// Store stores the dependencies
+	Store struct {
+		db *sqlx.DB
+	}
+
+	Player struct {
+		ID          int         `db:"id" json:"id"`
+		Name        string      `db:"name" json:"name"`
+		Image       null.String `db:"image" json:"image"`
+		FileName    null.String `json:"file_name"`
+		TempDOB     int64       `db:"date_of_birth" json:"date_of_birth"`
+		DateOfBirth null.Time
+		Position    null.String `db:"position" json:"position"`
+		IsCaptain   bool        `db:"captain" json:"is_captain"`
+		TeamID      int         `db:"team_id" json:"team_id"`
+	}
+)
+
+// NewPlayerRepo stores our dependency
+func NewPlayerRepo(db *sqlx.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetPlayers(ctx context.Context) ([]Player, error) {
+	return s.getPlayers(ctx)
+}
+
+func (s *Store) GetPlayersTeam(ctx context.Context, teamID int) ([]Player, error) {
+	return s.getPlayersTeam(ctx, teamID)
+}
+
+func (s *Store) GetPlayer(ctx context.Context, i Player) (Player, error) {
+	return s.getPlayer(ctx, i)
+}
+
+func (s *Store) AddPlayer(ctx context.Context, i Player) (Player, error) {
+	return s.addPlayer(ctx, i)
+}
+
+func (s *Store) EditPlayer(ctx context.Context, i Player) (Player, error) {
+	return s.editPlayer(ctx, i)
+}
+
+func (s *Store) DeletePlayer(ctx context.Context, i Player) error {
+	return s.deletePlayer(ctx, i)
+}
