daf4056a13c994d6f1bd1c09b63593073f153be7
Liam Burnand <liam.burnand@ystv.co.uk>
2023-10-22 19:31:03 +0100

    Adding initial database stuff for User

---
+++ user/db.go
@@ -0,0 +1,149 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"github.com/COMTOP1/AFC-GO/utils"
+	sq "github.com/Masterminds/squirrel"
+	"strconv"
+)
+
+func (s *Store) getUsers(ctx context.Context) ([]User, error) {
+	var p []User
+	builder := sq.Select("id", "name", "email", "phone", "team_id", "role", "image", "file_name").
+		From("afc.users").
+		OrderBy("id")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getUsers: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &p, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %w", err)
+	}
+	return p, nil
+}
+
+func (s *Store) getUsersContact(ctx context.Context) ([]User, error) {
+	var p []User
+	builder := sq.Select("id", "name", "email", "role").
+		From("afc.users").
+		Where("role IN ('PROGRAMME_EDITOR', 'LEAGUE_SECRETARY', 'TREASURER', 'SAFEGUARDING_OFFICER', 'CLUB_SECRETARY', 'CHAIRPERSON')").
+		OrderBy("FIELD(role, 'PROGRAMME_EDITOR', 'LEAGUE_SECRETARY', 'TREASURER', 'SAFEGUARDING_OFFICER', 'CLUB_SECRETARY', 'CHAIRPERSON') DESC")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getUsersContact: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &p, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %w", err)
+	}
+	return p, nil
+}
+
+func (s *Store) getUsersManagersTeam(ctx context.Context, teamID int) ([]User, error) {
+	var p []User
+	builder := sq.Select("id", "name", "image", "file_name").
+		From("afc.users").
+		Where(sq.Eq{"team_id": strconv.FormatUint(uint64(teamID), 10)}).
+		OrderBy("name")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getUsersManagersTeam: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &p, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %w", err)
+	}
+	return p, nil
+}
+
+func (s *Store) getUser(ctx context.Context, p User) (User, error) {
+	var p1 User
+	builder := sq.Select("id", "name", "email", "phone", "team_id", "role", "image", "file_name").
+		From("afc.users").
+		Where(sq.Eq{"id": p.ID})
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getUser: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &p1, sql)
+	if err != nil {
+		return User{}, fmt.Errorf("failed to get user: %w", err)
+	}
+	return p1, nil
+}
+
+func (s *Store) addUser(ctx context.Context, u User) (User, error) {
+	builder := utils.MySQL().Insert("afc.users").
+		Columns("name", "email", "phone", "team_id", "role", "image", "file_name").
+		Values(u.Name, u.Email, u.Phone, u.TeamID, u.Role.DBString(), u.Image, u.FileName)
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for addUser: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return User{}, fmt.Errorf("failed to add user: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return User{}, fmt.Errorf("failed to add user: %w", err)
+	}
+	if rows < 1 {
+		return User{}, fmt.Errorf("failed to add user: invalid rows affected: %d", rows)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return User{}, fmt.Errorf("failed to add user: %w", err)
+	}
+	u.ID = int(id)
+	return u, nil
+}
+
+func (s *Store) editUser(ctx context.Context, u User) (User, error) {
+	builder := utils.MySQL().Update("afc.users").
+		SetMap(map[string]interface{}{
+			"name":      u.Name,
+			"email":     u.Email,
+			"phone":     u.Phone,
+			"team_id":   u.TeamID,
+			"role":      u.Role.DBString(),
+			"image":     u.Image,
+			"file_name": u.FileName,
+			"password":  u.Password,
+			"hash":      u.Hash,
+			"salt":      u.Salt,
+		}).
+		Where(sq.Eq{"id": u.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for editUser: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return User{}, fmt.Errorf("failed to edit user: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return User{}, fmt.Errorf("failed to edit user: %w", err)
+	}
+	if rows < 1 {
+		return User{}, fmt.Errorf("failed to edit user: invalid rows affected: %d, this user may not exist: %d", rows, u.ID)
+	}
+	return u, nil
+}
+
+func (s *Store) deleteUser(ctx context.Context, u User) error {
+	builder := utils.MySQL().Delete("afc.users").
+		Where(sq.Eq{"id": u.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for deleteUser: %w", err))
+	}
+	_, err = s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+	return nil
+}
---
+++ user/user.go
@@ -0,0 +1,66 @@
+package user
+
+import (
+	"context"
+	"github.com/COMTOP1/AFC-GO/role"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/guregu/null.v4"
+)
+
+type (
+	// Store stores the dependencies
+	Store struct {
+		db *sqlx.DB
+	}
+
+	User struct {
+		ID            int      `db:"id" json:"id"`
+		Name          string   `db:"name" json:"name"`
+		Email         string   `db:"email" json:"email"`
+		Phone         string   `db:"phone" json:"phone"`
+		TeamID        null.Int `db:"team_id" json:"team_id"`
+		TempRole      string   `db:"role" json:"role"`
+		Role          role.Role
+		Image         null.String `db:"image" json:"image"`
+		FileName      null.String `db:"file_name" json:"file_name"`
+		Password      null.String `db:"password" json:"password"`
+		Hash          null.String `db:"hash" json:"hash"`
+		Salt          null.String `db:"salt" json:"salt"`
+		Authenticated bool
+	}
+)
+
+// NewUserRepo stores our dependency
+func NewUserRepo(db *sqlx.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetUsers(ctx context.Context) ([]User, error) {
+	return s.getUsers(ctx)
+}
+
+func (s *Store) GetUsersContact(ctx context.Context) ([]User, error) {
+	return s.getUsersContact(ctx)
+}
+
+func (s *Store) GetUsersManagersTeam(ctx context.Context, teamID int) ([]User, error) {
+	return s.getUsersManagersTeam(ctx, teamID)
+}
+
+func (s *Store) GetUser(ctx context.Context, p User) (User, error) {
+	return s.getUser(ctx, p)
+}
+
+func (s *Store) AddUser(ctx context.Context, p User) (User, error) {
+	return s.addUser(ctx, p)
+}
+
+func (s *Store) EditUser(ctx context.Context, p User) (User, error) {
+	return s.editUser(ctx, p)
+}
+
+func (s *Store) DeleteUser(ctx context.Context, p User) error {
+	return s.deleteUser(ctx, p)
+}
