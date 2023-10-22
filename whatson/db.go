9c456009bf2244b1132aa98fc48609b7b9963f9e
Liam Burnand <liam.burnand@ystv.co.uk>
2023-10-22 19:31:16 +0100

    Adding initial database stuff for What's On

---
+++ whatson/db.go
@@ -0,0 +1,165 @@
+package whatson
+
+import (
+	"context"
+	"fmt"
+	"github.com/COMTOP1/AFC-GO/utils"
+	sq "github.com/Masterminds/squirrel"
+	"time"
+)
+
+func (s *Store) getWhatsOnS(ctx context.Context) ([]WhatsOn, error) {
+	var w []WhatsOn
+	builder := sq.Select("id", "title", "image", "file_name", "content", "date", "date_of_event").
+		From("afc.whatson").
+		OrderBy("id")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getWhatsOns: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &w, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get what's on: %w", err)
+	}
+	return w, nil
+}
+
+func (s *Store) getWhatsOnFuture(ctx context.Context) ([]WhatsOn, error) {
+	var w []WhatsOn
+	builder := sq.Select("id", "title", "image", "file_name", "content", "date", "date_of_event").
+		From("afc.whatson").
+		Where(sq.GtOrEq{"date_of_event": time.Now().UnixMilli()}).
+		OrderBy("name")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getWhatsOnsSeason: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &w, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get what's on: %w", err)
+	}
+	return w, nil
+}
+
+func (s *Store) getWhatsOnPast(ctx context.Context) ([]WhatsOn, error) {
+	var w []WhatsOn
+	builder := sq.Select("id", "title", "image", "file_name", "content", "date", "date_of_event").
+		From("afc.whatson").
+		Where(sq.Lt{"date_of_event": time.Now().UnixMilli()}).
+		OrderBy("name")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getWhatsOnsSeason: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &w, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get what's on: %w", err)
+	}
+	return w, nil
+}
+
+func (s *Store) getWhatsOnLatest(ctx context.Context) (WhatsOn, error) {
+	temp := make([]WhatsOn, 1)
+	builder := sq.Select("id", "title", "image", "file_name", "content", "date", "date_of_event").
+		From("afc.whatsOn").
+		OrderBy("id DESC").
+		Limit(1)
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getWhatsOn: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &temp, sql)
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to get what's on latest: %w", err)
+	}
+
+	var n1 WhatsOn
+	n1 = temp[0]
+	return n1, nil
+}
+
+func (s *Store) getWhatsOn(ctx context.Context, w WhatsOn) (WhatsOn, error) {
+	var w1 WhatsOn
+	builder := sq.Select("id", "title", "image", "file_name", "content", "date", "date_of_event").
+		From("afc.whatson").
+		Where(sq.Eq{"id": w.ID})
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getWhatsOn: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &w1, sql)
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to get what's on: %w", err)
+	}
+	return w1, nil
+}
+
+func (s *Store) addWhatsOn(ctx context.Context, w WhatsOn) (WhatsOn, error) {
+	builder := utils.MySQL().Insert("afc.whatson").
+		Columns("title", "image", "file_name", "content", "date", "date_of_event").
+		Values(w.Title, w.Image, w.FileName, w.Content, w.Date, w.DateOfEvent)
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for addWhatsOn: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to add what's on: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to add what's on: %w", err)
+	}
+	if rows < 1 {
+		return WhatsOn{}, fmt.Errorf("failed to add what's on: invalid rows affected: %d", rows)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to add what's on: %w", err)
+	}
+	w.ID = int(id)
+	return w, nil
+}
+
+func (s *Store) editWhatsOn(ctx context.Context, w WhatsOn) (WhatsOn, error) {
+	builder := utils.MySQL().Update("afc.whatson").
+		SetMap(map[string]interface{}{
+			"title":         w.Title,
+			"image":         w.Image,
+			"file_name":     w.FileName,
+			"content":       w.Content,
+			"date":          w.TempDate,
+			"date_of_event": w.TempDOE,
+		}).
+		Where(sq.Eq{"id": w.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for editWhatsOn: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to edit what's on: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return WhatsOn{}, fmt.Errorf("failed to edit what's on: %w", err)
+	}
+	if rows < 1 {
+		return WhatsOn{}, fmt.Errorf("failed to edit what's on: invalid rows affected: %d, this what's on may not exist: %d", rows, w.ID)
+	}
+	return w, nil
+}
+
+func (s *Store) deleteWhatsOn(ctx context.Context, w WhatsOn) error {
+	builder := utils.MySQL().Delete("afc.whatson").
+		Where(sq.Eq{"id": w.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for deleteWhatsOn: %w", err))
+	}
+	_, err = s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("failed to delete what's on: %w", err)
+	}
+	return nil
+}
---
+++ whatson/whatson.go
@@ -0,0 +1,66 @@
+package whatson
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/guregu/null.v4"
+	"time"
+)
+
+type (
+	// Store stores the dependencies
+	Store struct {
+		db *sqlx.DB
+	}
+
+	WhatsOn struct {
+		ID          int         `db:"id" json:"id"`
+		Title       string      `db:"title" json:"title"`
+		Image       null.String `db:"image" json:"image"`
+		FileName    string      `db:"file_name" json:"file_name"`
+		Content     string      `db:"content" json:"content"`
+		TempDate    int64       `db:"date" json:"date"`
+		Date        time.Time
+		TempDOE     int64 `db:"date_of_event" json:"date_of_event"`
+		DateOfEvent time.Time
+	}
+)
+
+// NewWhatsOnRepo stores our dependency
+func NewWhatsOnRepo(db *sqlx.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetWhatsOnS(ctx context.Context) ([]WhatsOn, error) {
+	return s.getWhatsOnS(ctx)
+}
+
+func (s *Store) GetWhatsOnFuture(ctx context.Context) ([]WhatsOn, error) {
+	return s.getWhatsOnFuture(ctx)
+}
+
+func (s *Store) GetWhatsOnPast(ctx context.Context) ([]WhatsOn, error) {
+	return s.getWhatsOnPast(ctx)
+}
+
+func (s *Store) GetWhatsOnLatest(ctx context.Context) (WhatsOn, error) {
+	return s.getWhatsOnLatest(ctx)
+}
+
+func (s *Store) GetWhatsOn(ctx context.Context, i WhatsOn) (WhatsOn, error) {
+	return s.getWhatsOn(ctx, i)
+}
+
+func (s *Store) AddWhatsOn(ctx context.Context, i WhatsOn) (WhatsOn, error) {
+	return s.addWhatsOn(ctx, i)
+}
+
+func (s *Store) EditWhatsOn(ctx context.Context, i WhatsOn) (WhatsOn, error) {
+	return s.editWhatsOn(ctx, i)
+}
+
+func (s *Store) DeleteWhatsOn(ctx context.Context, i WhatsOn) error {
+	return s.deleteWhatsOn(ctx, i)
+}
