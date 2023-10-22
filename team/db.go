89500499f805b5f5a5eeae76a7ca76191efb4382
Liam Burnand <liam.burnand@ystv.co.uk>
2023-10-22 19:30:49 +0100

    Adding initial database stuff for Team

---
+++ team/db.go
@@ -0,0 +1,133 @@
+package team
+
+import (
+	"context"
+	"fmt"
+	"github.com/COMTOP1/AFC-GO/utils"
+	sq "github.com/Masterminds/squirrel"
+)
+
+func (s *Store) getTeams(ctx context.Context) ([]Team, error) {
+	var t []Team
+	builder := sq.Select("id", "name", "league", "division", "league_table", "fixtures", "coach", "physio", "image", "file_name", "active", "youth", "ages").
+		From("afc.teams").
+		OrderBy("id")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getTeams: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &t, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get teams: %w", err)
+	}
+	return t, nil
+}
+
+func (s *Store) getTeamsActive(ctx context.Context) ([]Team, error) {
+	var t []Team
+	builder := sq.Select("id", "name", "league", "division", "league_table", "fixtures", "coach", "physio", "image", "file_name", "active", "youth", "ages").
+		From("afc.teams").
+		Where(sq.Eq{"active": true}).
+		OrderBy("name")
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getTeamsSeason: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &t, sql)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get teams: %w", err)
+	}
+	return t, nil
+}
+
+func (s *Store) getTeam(ctx context.Context, t Team) (Team, error) {
+	var t1 Team
+	builder := sq.Select("id", "name", "league", "division", "league_table", "fixtures", "coach", "physio", "image", "file_name", "active", "youth", "ages").
+		From("afc.teams").
+		Where(sq.Eq{"id": t.ID})
+	sql, _, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for getTeam: %w", err))
+	}
+	err = s.db.SelectContext(ctx, &t1, sql)
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to get team: %w", err)
+	}
+	return t1, nil
+}
+
+func (s *Store) addTeam(ctx context.Context, t Team) (Team, error) {
+	builder := utils.MySQL().Insert("afc.teams").
+		Columns("name", "league", "division", "league_table", "fixtures", "coach", "physio", "image", "file_name", "active", "youth", "ages").
+		Values(t.Name, t.League, t.Division, t.LeagueTable, t.Fixtures, t.Coach, t.Physio, t.Image, t.FileName, t.IsActive, t.IsYouth, t.Ages)
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for addTeam: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to add team: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to add team: %w", err)
+	}
+	if rows < 1 {
+		return Team{}, fmt.Errorf("failed to add team: invalid rows affected: %d", rows)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to add team: %w", err)
+	}
+	t.ID = int(id)
+	return t, nil
+}
+
+func (s *Store) editTeam(ctx context.Context, t Team) (Team, error) {
+	builder := utils.MySQL().Update("afc.teams").
+		SetMap(map[string]interface{}{
+			"name":         t.Name,
+			"league":       t.League,
+			"division":     t.Division,
+			"league_table": t.LeagueTable,
+			"fixtures":     t.Fixtures,
+			"coach":        t.Coach,
+			"physio":       t.Physio,
+			"image":        t.Image,
+			"file_name":    t.FileName,
+			"active":       t.IsActive,
+			"youth":        t.IsYouth,
+			"ages":         t.Ages,
+		}).
+		Where(sq.Eq{"id": t.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for editTeam: %w", err))
+	}
+	res, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to edit team: %w", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return Team{}, fmt.Errorf("failed to edit team: %w", err)
+	}
+	if rows < 1 {
+		return Team{}, fmt.Errorf("failed to edit team: invalid rows affected: %d, this team may not exist: %d", rows, t.ID)
+	}
+	return t, nil
+}
+
+func (s *Store) deleteTeam(ctx context.Context, t Team) error {
+	builder := utils.MySQL().Delete("afc.teams").
+		Where(sq.Eq{"id": t.ID})
+	sql, args, err := builder.ToSql()
+	if err != nil {
+		panic(fmt.Errorf("failed to build sql for deleteTeam: %w", err))
+	}
+	_, err = s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("failed to delete team: %w", err)
+	}
+	return nil
+}
---
+++ team/team.go
@@ -0,0 +1,61 @@
+package team
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/guregu/null.v4"
+)
+
+type (
+	// Store stores the dependencies
+	Store struct {
+		db *sqlx.DB
+	}
+
+	Team struct {
+		ID          int         `db:"id" json:"id"`
+		Name        string      `db:"name" json:"name"`
+		League      string      `db:"league" json:"league"`
+		Division    string      `db:"division" json:"division"`
+		LeagueTable null.String `db:"league_table" json:"league_table"`
+		Fixtures    null.String `db:"fixtures" json:"fixtures"`
+		Coach       string      `db:"coach" json:"coach"`
+		Physio      null.String `db:"physio" json:"physio"`
+		Image       null.String `db:"team_photo" json:"image"`
+		FileName    string      `db:"file_name" json:"file_name"`
+		IsActive    bool        `db:"active" json:"is_active"`
+		IsYouth     bool        `db:"youth" json:"is_youth"`
+		Ages        int         `db:"ages" json:"ages"`
+	}
+)
+
+// NewTeamRepo stores our dependency
+func NewTeamRepo(db *sqlx.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetTeams(ctx context.Context) ([]Team, error) {
+	return s.getTeams(ctx)
+}
+
+func (s *Store) GetTeamsActive(ctx context.Context) ([]Team, error) {
+	return s.getTeamsActive(ctx)
+}
+
+func (s *Store) GetTeam(ctx context.Context, t Team) (Team, error) {
+	return s.getTeam(ctx, t)
+}
+
+func (s *Store) AddTeam(ctx context.Context, t Team) (Team, error) {
+	return s.addTeam(ctx, t)
+}
+
+func (s *Store) EditTeam(ctx context.Context, t Team) (Team, error) {
+	return s.editTeam(ctx, t)
+}
+
+func (s *Store) DeleteTeam(ctx context.Context, t Team) error {
+	return s.deleteTeam(ctx, t)
+}
