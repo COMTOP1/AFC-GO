1aaea41fd7ec9997e951f9afa60871858029e7a6
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-10-30 18:30:33 +0000

    Adding in parameters to pass to errors page

--- views/errors.go
+++ views/errors.go
@@ -2,13 +2,16 @@ package views
 
 import (
 	"errors"
+	"github.com/COMTOP1/AFC-GO/user"
 	"log"
+	"time"
 
 	"github.com/COMTOP1/AFC-GO/templates"
 	"github.com/labstack/echo/v4"
 )
 
 func (v *Views) CustomHTTPErrorHandler(err error, c echo.Context) {
+	c1 := v.getSessionData(c)
 	log.Print(err)
 	var he *echo.HTTPError
 	var status int
@@ -22,13 +25,18 @@ func (v *Views) CustomHTTPErrorHandler(err error, c echo.Context) {
 	if he != nil {
 		message = he.Message
 	}
+	year, _, _ := time.Now().Date()
 	c.Response().WriteHeader(status)
 	data := struct {
 		Code  int
 		Error any
+		Year  int
+		User  user.User
 	}{
 		Code:  status,
 		Error: message,
+		Year:  year,
+		User:  c1.User,
 	}
 	err1 := v.template.RenderTemplate(c.Response().Writer, data, templates.ErrorTemplate, templates.RegularType)
 	if err1 != nil {
