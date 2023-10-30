bcc3dcec5d4c0192bafd11c8ad450e36a783c584
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-10-30 21:34:03 +0000

    Adding checks if the file existed otherwise it appears as a 404

--- views/download.go
+++ views/download.go
@@ -2,8 +2,10 @@ package views
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 
@@ -47,7 +49,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(affiliation.FileName.String) == 0 || !affiliation.FileName.Valid {
 			return fmt.Errorf("download failed to get affiliation file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, affiliation.FileName.String), affiliation.FileName.String)
+		path := filepath.Join(v.conf.FileDir, affiliation.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for affiliation download: no such file")
+			}
+			return fmt.Errorf("failed to get file for affiliation download: %w", err)
+		}
+		return c.Inline(path, affiliation.FileName.String)
 	case "d": // Document
 		var document document1.Document
 		document, err = v.document.GetDocument(c.Request().Context(), document1.Document{ID: id})
@@ -57,10 +67,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(document.FileName) == 0 {
 			return fmt.Errorf("download failed to get document file name: no file name is present")
 		}
-		if len(document.FileName) == 0 {
-			return fmt.Errorf("download failed to get document file name: no file name is present")
+		path := filepath.Join(v.conf.FileDir, document.FileName)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for document download: no such file")
+			}
+			return fmt.Errorf("failed to get file for document download: %w", err)
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, document.FileName), document.FileName)
+		return c.Inline(path, document.FileName)
 	case "g": // Gallery
 		var image image1.Image
 		image, err = v.image.GetImage(c.Request().Context(), image1.Image{ID: id})
@@ -70,7 +85,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(image.FileName.String) == 0 {
 			return fmt.Errorf("download failed to get image file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, image.FileName.String), image.FileName.String)
+		path := filepath.Join(v.conf.FileDir, image.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for image download: no such file")
+			}
+			return fmt.Errorf("failed to get file for image download: %w", err)
+		}
+		return c.Inline(path, image.FileName.String)
 	case "l": // Player
 		var player player1.Player
 		player, err = v.player.GetPlayer(c.Request().Context(), player1.Player{ID: id})
@@ -80,7 +103,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(player.FileName.String) == 0 || !player.FileName.Valid {
 			return fmt.Errorf("download failed to get player file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, player.FileName.String), player.FileName.String)
+		path := filepath.Join(v.conf.FileDir, player.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for player download: no such file")
+			}
+			return fmt.Errorf("failed to get file for player download: %w", err)
+		}
+		return c.Inline(path, player.FileName.String)
 	case "n": // News
 		var news news1.News
 		news, err = v.news.GetNewsArticle(c.Request().Context(), news1.News{ID: id})
@@ -90,7 +121,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(news.FileName.String) == 0 || !news.FileName.Valid {
 			return fmt.Errorf("download failed to get news file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, news.FileName.String), news.FileName.String)
+		path := filepath.Join(v.conf.FileDir, news.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for news download: no such file")
+			}
+			return fmt.Errorf("failed to get file for news download: %w", err)
+		}
+		return c.Inline(path, news.FileName.String)
 	case "p": // Programme
 		var programme programme1.Programme
 		programme, err = v.programme.GetProgramme(c.Request().Context(), programme1.Programme{ID: id})
@@ -100,7 +139,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(programme.FileName) == 0 {
 			return fmt.Errorf("download failed to get programme file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, programme.FileName), programme.FileName)
+		path := filepath.Join(v.conf.FileDir, programme.FileName)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for programme download: no such file")
+			}
+			return fmt.Errorf("failed to get file for programme download: %w", err)
+		}
+		return c.Inline(path, programme.FileName)
 	case "s": // Sponsor
 		var sponsor sponsor1.Sponsor
 		sponsor, err = v.sponsor.GetSponsor(c.Request().Context(), sponsor1.Sponsor{ID: id})
@@ -110,7 +157,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(sponsor.FileName.String) == 0 || !sponsor.FileName.Valid {
 			return fmt.Errorf("download failed to get sponsor file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, sponsor.FileName.String), sponsor.FileName.String)
+		path := filepath.Join(v.conf.FileDir, sponsor.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for sponsor download: no such file")
+			}
+			return fmt.Errorf("failed to get file for sponsor download: %w", err)
+		}
+		return c.Inline(path, sponsor.FileName.String)
 	case "t": // Team
 		var team team1.Team
 		team, err = v.team.GetTeam(c.Request().Context(), team1.Team{ID: id})
@@ -120,7 +175,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(team.FileName.String) == 0 || !team.FileName.Valid {
 			return fmt.Errorf("download failed to get team file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, team.FileName.String), team.FileName.String)
+		path := filepath.Join(v.conf.FileDir, team.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for team download: no such file")
+			}
+			return fmt.Errorf("failed to get file for team download: %w", err)
+		}
+		return c.Inline(path, team.FileName.String)
 	case "u": // User
 		var user user1.User
 		user, err = v.user.GetUser(c.Request().Context(), user1.User{ID: id})
@@ -130,7 +193,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(user.FileName.String) == 0 || !user.FileName.Valid {
 			return fmt.Errorf("download failed to get user file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, user.FileName.String), user.FileName.String)
+		path := filepath.Join(v.conf.FileDir, user.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for user download: no such file")
+			}
+			return fmt.Errorf("failed to get file for user download: %w", err)
+		}
+		return c.Inline(path, user.FileName.String)
 	case "w": // WhatsOn
 		var whatsOn whatson1.WhatsOn
 		whatsOn, err = v.whatsOn.GetWhatsOnArticle(c.Request().Context(), whatson1.WhatsOn{ID: id})
@@ -140,7 +211,15 @@ func (v *Views) Download(c echo.Context) error {
 		if len(whatsOn.FileName.String) == 0 || !whatsOn.FileName.Valid {
 			return fmt.Errorf("download failed to get what's on file name: no file name is present")
 		}
-		return c.Inline(filepath.Join(v.conf.FileDir, whatsOn.FileName.String), whatsOn.FileName.String)
+		path := filepath.Join(v.conf.FileDir, whatsOn.FileName.String)
+		_, err = os.Stat(path)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file") {
+				return fmt.Errorf("failed to get file for whatson download: no such file")
+			}
+			return fmt.Errorf("failed to get file for whatson download: %w", err)
+		}
+		return c.Inline(path, whatsOn.FileName.String)
 	default:
 		return fmt.Errorf("download failed to get source: source format does not conform")
 	}
