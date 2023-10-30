f98a039d2c5b402fbeefc9d1de1eb197a9a87402
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-10-30 18:30:56 +0000

    Adding info backend

--- views/info.go
+++ views/info.go
@@ -1,1 +1,28 @@
 package views
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/COMTOP1/AFC-GO/templates"
+	"github.com/COMTOP1/AFC-GO/user"
+)
+
+func (v *Views) InfoFunc(c echo.Context) error {
+	c1 := v.getSessionData(c)
+
+	year, _, _ := time.Now().Date()
+
+	_ = c1.User
+
+	data := struct {
+		Year int
+		User user.User
+	}{
+		Year: year,
+		User: c1.User,
+	}
+
+	return v.template.RenderTemplate(c.Response().Writer, data, templates.InfoTemplate, templates.RegularType)
+}
