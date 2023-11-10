235e0d565fc8c3e3c6b7b6787c57c181ba8bd73b
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-11-10 02:43:22 +0000

    Adding initial programme.go code

--- views/programme.go
+++ views/programme.go
@@ -1,1 +1,37 @@
 package views
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (v *Views) ProgrammesFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) ProgrammeAddFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) ProgrammeDeleteFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) ProgrammeSeasonAddFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) ProgrammeSeasonEditFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) ProgrammeSeasonDeleteFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
