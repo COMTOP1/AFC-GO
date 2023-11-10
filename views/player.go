5b845c3a0df757c8f3f3e44c6c47ffab1bfb5358
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-11-10 02:43:10 +0000

    Adding initial player.go code

--- views/player.go
+++ views/player.go
@@ -1,1 +1,27 @@
 package views
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (v *Views) PlayersFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) PlayerAddFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) PlayerEditFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
+
+func (v *Views) PlayerDeleteFunc(c echo.Context) error {
+	_ = c
+	return fmt.Errorf("not implemented yet")
+}
