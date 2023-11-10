bf81b64f836c35617865a0f0346fdc79c05d37af
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-11-10 02:44:24 +0000

    Adding new middleware functions

--- views/middleware.go
+++ views/middleware.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"github.com/COMTOP1/AFC-GO/role"
 )
 
 // RequiresLogin is a middleware which will be used for each
@@ -44,84 +46,36 @@ func (v *Views) RequiresLogin(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
-//// RequiresLoginJSON is a middleware which is used for each
-//// httpHandler to check if there is any active session and returns json if not
-//func (v *Views) RequiresLoginJSON(next echo.HandlerFunc) echo.HandlerFunc {
-//	return func(c echo.Context) error {
-//		session, err := v.cookie.Get(c.Request(), v.conf.SessionCookieName)
-//		if err != nil {
-//			log.Printf("failed to get session for requiresLoginJSON: %+v", err)
-//			data := struct {
-//				Error error `json:"error"`
-//			}{
-//				Error: err,
-//			}
-//			return c.JSON(http.StatusInternalServerError, data)
-//		}
-//		c1 := v.getSessionData(c)
-//		if !c1.User.Authenticated {
-//			data := struct {
-//				Error string `json:"error"`
-//			}{
-//				Error: "user not logged in",
-//			}
-//			return c.JSON(http.StatusUnauthorized, data)
-//		}
-//		userFromDB, err := v.user.GetUser(c.Request().Context(), c1.User)
-//		if err != nil {
-//			log.Printf("failed to get user for requiresLoginJSON: %+v", err)
-//			data := struct {
-//				Error error `json:"error"`
-//			}{
-//				Error: fmt.Errorf("failed to get user for requiresLoginJSON: %w", err),
-//			}
-//			return c.JSON(http.StatusInternalServerError, data)
-//		}
-//		if userFromDB.DeletedBy.Valid || !c1.User.Enabled {
-//			session.Values["user"] = &user.User{}
-//			session.Options.MaxAge = -1
-//			err = session.Save(c.Request(), c.Response())
-//			if err != nil {
-//				log.Printf("failed to save session for requiresLoginJSON: %+v", err)
-//				data := struct {
-//					Error error `json:"error"`
-//				}{
-//					Error: fmt.Errorf("failed to save session for requiresLoginJSON: %w", err),
-//				}
-//				return c.JSON(http.StatusInternalServerError, data)
-//			}
-//			data := struct {
-//				Error string `json:"error"`
-//			}{
-//				Error: "user deleted or not enabled",
-//			}
-//			return c.JSON(http.StatusUnauthorized, data)
-//		}
-//		return next(c)
-//	}
-//}
-//
-//func (v *Views) RequirePermission(r role.Role) echo.MiddlewareFunc {
-//	return func(next echo.HandlerFunc) echo.HandlerFunc {
-//		return func(c echo.Context) error {
-//			c1 := v.getSessionData(c)
-//			if c1 == nil {
-//				return fmt.Errorf("failed to get session data")
-//			}
-//			perms, err := v.user.GetPermissionsForUser(c.Request().Context(), c1.User)
-//			if err != nil {
-//				return fmt.Errorf("failed to get permissions for requirePermission: %w", err)
-//			}
-//
-//			acceptedPerms := role1.SufficientPermissionsFor(r)
-//
-//			for _, perm := range perms {
-//				if acceptedPerms[perm.Name] {
-//					return next(c)
-//				}
-//			}
-//
-//			return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you are not authorised for accessing this"))
-//		}
-//	}
-//}
+func (v *Views) RequireNotManager(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c1 := v.getSessionData(c)
+		if c1 == nil {
+			return fmt.Errorf("failed to get session data")
+		}
+
+		if c1.User.ID > 0 && c1.User.Role != role.Manager {
+			return next(c)
+		}
+
+		return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you are not authorised for accessing this"))
+	}
+}
+
+func (v *Views) RequireClubSecretaryHigher(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c1 := v.getSessionData(c)
+		if c1 == nil {
+			return fmt.Errorf("failed to get session data")
+		}
+
+		if c1.User.ID <= 0 {
+			return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you are not authorised for accessing this"))
+		}
+
+		if c1.User.Role == role.ClubSecretary || c1.User.Role == role.Chairperson || c1.User.Role == role.Webmaster {
+			return next(c)
+		}
+
+		return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you are not authorised for accessing this"))
+	}
+}
