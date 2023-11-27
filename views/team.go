52bc54ecbdf65dcbb0937595b9436f3385cb134a
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-11-27 20:54:35 +0000

    Adding team add function

--- views/team.go
+++ views/team.go
@@ -2,11 +2,14 @@ package views
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"gopkg.in/guregu/null.v4"
 
 	"github.com/COMTOP1/AFC-GO/team"
 	"github.com/COMTOP1/AFC-GO/templates"
@@ -92,8 +95,109 @@ func (v *Views) TeamFunc(c echo.Context) error {
 }
 
 func (v *Views) TeamAddFunc(c echo.Context) error {
-	_ = c
-	return fmt.Errorf("not implemented yet")
+	if c.Request().Method == http.MethodPost {
+		data := struct {
+			Error string `json:"error"`
+		}{
+			Error: "",
+		}
+
+		name := c.FormValue("name")
+		if len(name) < 0 {
+			log.Printf("name must contain a value for teamAdd")
+			data.Error = fmt.Sprintf("name must contain a value")
+			return c.JSON(http.StatusOK, data)
+		}
+
+		league := c.FormValue("league")
+		division := c.FormValue("division")
+
+		leagueTable := c.FormValue("leagueTable")
+		if len(leagueTable) > 0 {
+			_, err := url.ParseRequestURI(leagueTable)
+			if err != nil {
+				log.Printf("failed to parse leagueTable for teamAdd: %+v", err)
+				data.Error = fmt.Sprintf("failed to parse leagueTable for teamAdd: %+v", err)
+				return c.JSON(http.StatusOK, data)
+			}
+		}
+
+		fixtures := c.FormValue("fixtures")
+		if len(fixtures) > 0 {
+			_, err := url.ParseRequestURI(fixtures)
+			if err != nil {
+				log.Printf("failed to parse fixtures for teamAdd: %+v", err)
+				data.Error = fmt.Sprintf("failed to parse fixtures for teamAdd: %+v", err)
+				return c.JSON(http.StatusOK, data)
+			}
+		}
+
+		tempCoach := c.FormValue("tempCoach")
+		coach := null.StringFrom(tempCoach)
+		if len(tempCoach) == 0 {
+			coach.Valid = false
+		}
+
+		tempPhysio := c.FormValue("physio")
+		physio := null.StringFrom(tempPhysio)
+		if len(tempPhysio) == 0 {
+			physio.Valid = false
+		}
+
+		var isActive, isYouth bool
+
+		tempIsActive := c.FormValue("isActive")
+		if tempIsActive == "Y" {
+			isActive = true
+		} else if len(tempIsActive) != 0 {
+			log.Printf("failed to parse isActive for teamAdd: %s", tempIsActive)
+			data.Error = fmt.Sprintf("failed to parse isActive for teamAdd: %s", tempIsActive)
+			return c.JSON(http.StatusOK, data)
+		}
+
+		tempIsYouth := c.FormValue("isYouth")
+		if tempIsYouth == "Y" {
+			isActive = true
+		} else if len(tempIsYouth) != 0 {
+			log.Printf("failed to parse isYouth for teamAdd: %s", tempIsYouth)
+			data.Error = fmt.Sprintf("failed to parse isYouth for teamAdd: %s", tempIsYouth)
+			return c.JSON(http.StatusOK, data)
+		}
+
+		ages, err := strconv.Atoi(c.FormValue("ages"))
+		if err != nil {
+			log.Printf("failed to parse ages for playerAdd: %+v", err)
+			data.Error = fmt.Sprintf("failed to parse ages for playerAdd: %+v", err)
+			return c.JSON(http.StatusOK, data)
+		}
+
+		if ages < 19 {
+			isYouth = true
+		}
+
+		file, err := c.FormFile("upload")
+		if err != nil {
+			log.Printf("failed to get file for teamAdd: %+v", err)
+			data.Error = fmt.Sprintf("failed to get file for teamAdd: %+v", err)
+			return c.JSON(http.StatusOK, data)
+		}
+		fileName, err := v.fileUpload(file)
+		if err != nil {
+			log.Printf("failed to upload file for teamAdd: %+v", err)
+			data.Error = fmt.Sprintf("failed to upload file for teamAdd: %+v", err)
+			return c.JSON(http.StatusOK, data)
+		}
+
+		_, err = v.team.AddTeam(c.Request().Context(), team.Team{Name: name, League: null.StringFrom(league), Division: null.StringFrom(division), LeagueTable: null.StringFrom(leagueTable), Fixtures: null.StringFrom(fixtures), Coach: coach, Physio: physio, FileName: null.StringFrom(fileName), IsActive: isActive, IsYouth: isYouth, Ages: ages})
+		if err != nil {
+			log.Printf("failed to add team for teamAdd: %+v", err)
+			data.Error = fmt.Sprintf("failed to add team for teamAdd: %+v", err)
+			return c.JSON(http.StatusOK, data)
+		}
+
+		return c.JSON(http.StatusOK, data)
+	}
+	return echo.NewHTTPError(http.StatusMethodNotAllowed, fmt.Errorf("invalid method used"))
 }
 
 func (v *Views) TeamEditFunc(c echo.Context) error {
