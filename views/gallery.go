67ad1419f36f52969c3bc86811f68f936b424ebf
Liam Burnand <liam.burnand@bswdi.co.uk>
2023-11-28 01:49:46 +0000

    Adding image delete function

--- views/gallery.go
+++ views/gallery.go
@@ -2,6 +2,10 @@ package views
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -40,6 +44,29 @@ func (v *Views) ImageAddFunc(c echo.Context) error {
 }
 
 func (v *Views) ImageDeleteFunc(c echo.Context) error {
-	_ = c
-	return fmt.Errorf("not implemented yet")
+	if c.Request().Method == http.MethodPost {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return fmt.Errorf("failed to get id for imageDelete: %w", err)
+		}
+
+		imageDB, err := v.image.GetImage(c.Request().Context(), image.Image{ID: id})
+		if err != nil {
+			return fmt.Errorf("failed to get image for imageDelete: %w", err)
+		}
+
+		if imageDB.FileName.Valid {
+			err = os.Remove(filepath.Join(v.conf.FileDir, imageDB.FileName.String))
+			if err != nil {
+				return fmt.Errorf("failed to delete image image for imageDelete: %w", err)
+			}
+		}
+
+		err = v.image.DeleteImage(c.Request().Context(), imageDB)
+		if err != nil {
+			return fmt.Errorf("failed to delete image for imageDelete: %w", err)
+		}
+		return c.Redirect(http.StatusFound, "/gallery")
+	}
+	return echo.NewHTTPError(http.StatusMethodNotAllowed, fmt.Errorf("invalid method used"))
 }
