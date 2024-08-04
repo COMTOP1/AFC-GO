8bd677ffedf2248e06793d8e654ba3ad0f5021f9
Liam Burnand <liam.burnand@bswdi.co.uk>
2024-08-04 09:52:59 +0100

    Improving error logging for affiliation.go

--- views/affilitaion.go
+++ views/affilitaion.go
@@ -1,7 +1,6 @@
 package views
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -32,29 +31,29 @@ func (v *Views) AffiliationAddFunc(c echo.Context) error {
 		if len(website) > 0 {
 			_, err := url.ParseRequestURI(website)
 			if err != nil {
-				log.Printf("failed to parse website for affiliationAdd: %+v", err)
-				data.Error = fmt.Sprintf("failed to parse website for affiliationAdd: %+v", err)
+				log.Printf("failed to parse website for affiliation add, error: %+v", err)
+				data.Error = fmt.Sprintf("failed to parse website for affiliation add: %+v", err)
 				return c.JSON(http.StatusOK, data)
 			}
 		}
 
 		file, err := c.FormFile("upload")
 		if err != nil {
-			log.Printf("failed to get file for affiliationAdd: %+v", err)
-			data.Error = fmt.Sprintf("failed to get file for affiliationAdd: %+v", err)
+			log.Printf("failed to get file for affiliation add, error: %+v", err)
+			data.Error = fmt.Sprintf("failed to get file for affiliation add: %+v", err)
 			return c.JSON(http.StatusOK, data)
 		}
 		fileName, err := v.fileUpload(file)
 		if err != nil {
-			log.Printf("failed to upload file for affiliationAdd: %+v", err)
-			data.Error = fmt.Sprintf("failed to upload file for affiliationAdd: %+v", err)
+			log.Printf("failed to upload file for affiliation add, error: %+v", err)
+			data.Error = fmt.Sprintf("failed to upload file for affiliation add: %+v", err)
 			return c.JSON(http.StatusOK, data)
 		}
 
 		_, err = v.affiliation.AddAffiliation(c.Request().Context(), affiliation.Affiliation{Name: name, Website: null.NewString(website, len(website) > 0), FileName: null.NewString(fileName, len(fileName) > 0)})
 		if err != nil {
-			log.Printf("failed to add affiliation for affiliationAdd: %+v", err)
-			data.Error = fmt.Sprintf("failed to add affiliation for affiliationAdd: %+v", err)
+			log.Printf("failed to add affiliation for affiliation add, error: %+v", err)
+			data.Error = fmt.Sprintf("failed to add affiliation for affiliation add: %+v", err)
 			return c.JSON(http.StatusOK, data)
 		}
 
@@ -62,13 +61,13 @@ func (v *Views) AffiliationAddFunc(c echo.Context) error {
 		c1.MsgType = "is-success"
 		err = v.setMessagesInSession(c, c1)
 		if err != nil {
-			log.Printf("failed to set data for affiliationAdd: %+v", err)
+			log.Printf("failed to set data for affiliation add, error: %+v", err)
 		}
 
 		return c.JSON(http.StatusOK, data)
 	}
 
-	return echo.NewHTTPError(http.StatusMethodNotAllowed, errors.New("invalid method used"))
+	return v.invalidMethodUsed(c)
 }
 
 func (v *Views) AffiliationDeleteFunc(c echo.Context) error {
@@ -77,34 +76,34 @@ func (v *Views) AffiliationDeleteFunc(c echo.Context) error {
 
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
-			return fmt.Errorf("failed to get id for affiliationDelete: %w", err)
+			return fmt.Errorf("failed to get id for affiliation delete, error: %w", err)
 		}
 
 		affiliationDB, err := v.affiliation.GetAffiliation(c.Request().Context(), affiliation.Affiliation{ID: id})
 		if err != nil {
-			return fmt.Errorf("failed to get affiliaiton for affiliationDelete: %w", err)
+			return fmt.Errorf("failed to get affiliaiton for affiliation delete, affiliation id: %d, error: %w", id, err)
 		}
 
 		if affiliationDB.FileName.Valid {
 			err = os.Remove(filepath.Join(v.conf.FileDir, affiliationDB.FileName.String))
 			if err != nil {
-				log.Printf("failed to delete affiliation image for affiliationDelete: %+v", err)
+				log.Printf("failed to delete affiliation image for affiliation delete, affiliation id: %d, error: %+v", id, err)
 			}
 		}
 
 		err = v.affiliation.DeleteAffiliation(c.Request().Context(), affiliationDB)
 		if err != nil {
-			return fmt.Errorf("failed to delete affiliaiton for affiliationDelete: %w", err)
+			return fmt.Errorf("failed to delete affiliaiton for affiliation delete, affiliation id: %d, error: %w", id, err)
 		}
 
 		c1.Message = fmt.Sprintf("successfully deleted \"%s\"", affiliationDB.Name)
 		c1.MsgType = "is-success"
 		err = v.setMessagesInSession(c, c1)
 		if err != nil {
-			log.Printf("failed to set data for affiliationDelete: %+v", err)
+			log.Printf("failed to set data for affiliation delete, affiliation id: %d, error: %+v", id, err)
 		}
 
 		return c.Redirect(http.StatusFound, "/")
 	}
-	return echo.NewHTTPError(http.StatusMethodNotAllowed, errors.New("invalid method used"))
+	return v.invalidMethodUsed(c)
 }
