56c0dfb5be2fa083ea1583df84f975ba938fb5c7
Liam Burnand <liam.burnand@bswdi.co.uk>
2025-02-21 22:13:44 +0000

    Changing the generating function

--- utils/hash.go
+++ utils/hash.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/sha512"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"math/big"
 
@@ -11,16 +12,28 @@ import (
 	"golang.org/x/crypto/scrypt"
 )
 
-type Type int
+type (
+	Type       int
+	Length     int
+	Characters string
+)
 
 const (
 	GeneratePassword Type = iota
 	GenerateSalt
+	GenerateUsername
 )
 
 const (
-	saltCharacters     = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/."
-	passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@*()&"
+	UsernameCharacters Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	SaltCharacters     Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/."
+	PasswordCharacters Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@*()&"
+)
+
+const (
+	PasswordLength Length = 12
+	SaltLength     Length = 22
+	UsernameLength Length = 20
 )
 
 func HashPass(password, salt []byte, iter, keyLen int) []byte {
@@ -35,37 +48,80 @@ func HashPassScrypt(password, salt []byte, workFactor, blockSize, parallelismFac
 	return hex.EncodeToString(hash), nil
 }
 
-// GenerateRandom generates a random string for either password or salt
-func GenerateRandom(t Type) (string, error) {
-	switch t {
+func GenerateRandomLength(length int, randomType Type) (string, error) {
+	if length < 6 || length > 40 {
+		return "", errors.New("length must be between 6 and 40")
+	}
+	switch randomType {
 	case GeneratePassword:
-		lenPass := big.NewInt(int64(len(passwordCharacters)))
-		b, err := rangeLoop(lenPass, 12)
+		b, err := rangeLoop(PasswordCharacters, Length(length))
 		if err != nil {
-			return "", fmt.Errorf("error generating random: %w", err)
+			return "", fmt.Errorf("error generating random password: %w", err)
 		}
+
 		return b, nil
 	case GenerateSalt:
-		lenSalt := big.NewInt(int64(len(saltCharacters)))
-		b, err := rangeLoop(lenSalt, 32)
+		b, err := rangeLoop(SaltCharacters, Length(length))
 		if err != nil {
-			return "", fmt.Errorf("error generating random: %w", err)
+			return "", fmt.Errorf("error generating random salt: %w", err)
 		}
+
+		return "$2a$06$" + b, nil
+	case GenerateUsername:
+		b, err := rangeLoop(UsernameCharacters, Length(length))
+		if err != nil {
+			return "", fmt.Errorf("error generating random username: %w", err)
+		}
+
 		return b, nil
 	default:
-		return "", fmt.Errorf("invalid type: %d", t)
+		return "", fmt.Errorf("invalid type: %d", randomType)
+	}
+}
+
+// GenerateRandom generates a random string for either password or salt
+func GenerateRandom(randomType Type) (string, error) {
+	switch randomType {
+	case GeneratePassword:
+		b, err := rangeLoop(PasswordCharacters, PasswordLength)
+		if err != nil {
+			return "", fmt.Errorf("error generating random password: %w", err)
+		}
+
+		return b, nil
+	case GenerateSalt:
+		b, err := rangeLoop(SaltCharacters, SaltLength)
+		if err != nil {
+			return "", fmt.Errorf("error generating random salt: %w", err)
+		}
+
+		return "$2a$06$" + b, nil
+	case GenerateUsername:
+		b, err := rangeLoop(UsernameCharacters, UsernameLength)
+		if err != nil {
+			return "", fmt.Errorf("error generating random username: %w", err)
+		}
+
+		return b, nil
+	default:
+		return "", fmt.Errorf("invalid type: %d", randomType)
 	}
 }
 
 // rangeLoop creates the random string that will be used
-func rangeLoop(len *big.Int, size int) (string, error) {
-	b := make([]byte, size)
-	for i := range b {
-		randInt, err := rand.Int(rand.Reader, len)
+func rangeLoop(characters Characters, randomLength Length) (string, error) {
+	bytes := make([]byte, randomLength)
+
+	length := big.NewInt(int64(len(characters)))
+
+	for i := range bytes {
+		randInt, err := rand.Int(rand.Reader, length)
 		if err != nil {
 			return "", err
 		}
-		b[i] = passwordCharacters[randInt.Int64()]
+
+		bytes[i] = characters[randInt.Int64()]
 	}
-	return string(b), nil
+
+	return string(bytes), nil
 }
